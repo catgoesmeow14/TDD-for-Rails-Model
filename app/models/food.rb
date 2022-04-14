be422051a182644195ee0271e0b623debaa78e40
catgoesmeow14 <exoexo1990@gmail.com>
2022-04-14 12:23:55 +0000

    Add validation only involves all foods that contain the 'N' letter in its name

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,9 @@
 class Food < ApplicationRecord
+  validates :description, presence: true
+  
   validates :name, presence: true, uniqueness: true
+
+  def self.by_letter(letter)
+    where("name LIKE ?", "#{letter}%").order(:name)
+  end
 end
